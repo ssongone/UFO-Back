54066eea8ebf8a4b2e29a59a7e44495e9785f0e2
ssongone <41146676+ssongone@users.noreply.github.com>
2023-11-13 20:19:27 +0900

    Initial commit

---
+++ src/test/java/jungle/spaceship/SpaceshipApplicationTests.java
@@ -0,0 +1,13 @@
+package jungle.spaceship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
