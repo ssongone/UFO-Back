568edfe7e83b19f0fa5e57b09cd08ee9d06c706c
ssongone <41146676+ssongone@users.noreply.github.com>
2023-11-13 22:11:24 +0900

    Feat: 멤버 엔티티 추가

---
+++ src/main/java/jungle/spaceship/entity/Member.java
@@ -0,0 +1,44 @@
+package jungle.spaceship.entity;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Member extends Timestamped{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = true)
+    private Role role;
+
+    @Builder
+    public Member(String name, String email, String picture, Role role){
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+
+}
