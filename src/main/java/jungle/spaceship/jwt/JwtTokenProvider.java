0fb37989ded9eefee5d5cd4966d70c7ee1297200
ssongone <41146676+ssongone@users.noreply.github.com>
2023-11-19 04:29:27 +0900

    Test: access token 만료시간 늘림 테스트용

--- src/main/java/jungle/spaceship/jwt/JwtTokenProvider.java
+++ src/main/java/jungle/spaceship/jwt/JwtTokenProvider.java
@@ -25,7 +25,7 @@ public class JwtTokenProvider {
     private final Key key;
     private static final String BEARER_TYPE = "Bearer";
     private static final String AUTHORITIES_KEY = "auth";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30 * 24 * 7;            // 30분인데 테스트 위해 늘림
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
 
     public JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
