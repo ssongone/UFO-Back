3f1d098a158c6a623e79acc804281a3ecfd8e008
zzicmuck <91383940+zzic-muck@users.noreply.github.com>
2023-11-25 01:48:09 +0900

    Fix: 안드로이드 채팅오류 수정

--- src/main/java/jungle/spaceship/config/WebSocketConfig.java
+++ src/main/java/jungle/spaceship/config/WebSocketConfig.java
@@ -6,9 +6,12 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 /**
  *  WebSocketConfig
@@ -19,10 +22,17 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
  */
 @Order(Ordered.HIGHEST_PRECEDENCE + 99)
 @Configuration
+@EnableWebSocket
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     private final WebSocketInterceptor webSocketInterceptor;
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.addDecoratorFactory(CustomWebSocketHandlerDecorator::new);
+    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config){
