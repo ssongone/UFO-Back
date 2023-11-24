a74d721b2a9db25b9f8f488bc3691e5777292d45
ssongone <41146676+ssongone@users.noreply.github.com>
2023-11-24 17:11:07 +0900

    Style: 가족역할 필드 Enum으로 변경 (title -> familyRole)

---
+++ src/main/java/jungle/spaceship/entity/FamilyRole.java
@@ -0,0 +1,25 @@
+package jungle.spaceship.entity;
+
+public enum FamilyRole {
+    DAD("아빠"),
+    MOM("엄마"),
+
+    FIRST("첫째"),
+    SECOND("둘째"),
+    THIRD("셋째"),
+    FOURTH("넷째"),
+    FIFTH("다섯째"),
+    SIXTH("여섯째"),
+
+    GRANDFATHER("할아버지"),
+    GRANDMOTHER("할머니"),
+
+    UNCLE("삼촌");
+
+
+    String roleName;
+
+    FamilyRole(String roleName) {
+        this.roleName = roleName;
+    }
+}
