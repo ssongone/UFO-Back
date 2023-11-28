0c1573e4a4640aee1f6aeb7baca66cf78377f1d0
ssongone <41146676+ssongone@users.noreply.github.com>
2023-11-28 20:48:54 +0900

    Test: FCM 테스트용 api 추가

--- src/main/java/jungle/spaceship/tmi/controller/TmiController.java
+++ src/main/java/jungle/spaceship/tmi/controller/TmiController.java
@@ -1,5 +1,10 @@
 package jungle.spaceship.tmi.controller;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import jungle.spaceship.jwt.SecurityUtil;
 import jungle.spaceship.response.BasicResponse;
 import jungle.spaceship.response.ExtendedResponse;
 import jungle.spaceship.tmi.controller.dto.TmiDto;
@@ -10,8 +15,12 @@ import jungle.spaceship.tmi.service.TmiService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -22,6 +31,9 @@ import java.util.Map;
 public class TmiController {
 
     private final TmiService tmiService;
+    private final SecurityUtil securityUtil;
+    private final FirebaseMessaging firebaseMessaging;
+
     @PostMapping("/tmi")
     public ResponseEntity<BasicResponse> registerTmi(@RequestBody TmiDto tmiDto){
         return ResponseEntity.ok(tmiService.registerTmi(tmiDto));
@@ -57,6 +69,31 @@ public class TmiController {
     @GetMapping("/weeklyAttendance")
     public ExtendedResponse<Map<Date, List<Attendance>>> weeklyAttendance() {
         return tmiService.weeklyAttendance();
+    }
+
+    @PostMapping("/mind627")
+    public String mind627() {
+        System.out.println("TmiController.mind627");
+        String token = securityUtil.extractMember().getFirebaseToken();
+
+        Notification notification = Notification.builder()
+                .setTitle("푸ㅅ1알➱림ㅌㅔ亼트✒")
+                .setBody("ε종윤ய은도H௩ヌ1❣")
+                .build();
+
+        Message message = Message.builder()
+                .setNotification(notification)
+                .putData("time", LocalDateTime.now().toString())
+                .putData("songwon", "배고픔")
+                .setToken(token).build();
+
+        try {
+            firebaseMessaging.send(message);
+        } catch (FirebaseMessagingException e) {
+            log.error("cannot send to memberList push message. error info : {}", e.getMessage());
+        }
+
+        return "완";
     }
 
 }
