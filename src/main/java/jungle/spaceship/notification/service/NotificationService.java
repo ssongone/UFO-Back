6fc86f7fdab9af03b06889ef43ad60ccd34fb766
ssongone <41146676+ssongone@users.noreply.github.com>
2023-11-30 16:36:30 +0900

    Fix: data null 체크

--- src/main/java/jungle/spaceship/notification/service/NotificationService.java
+++ src/main/java/jungle/spaceship/notification/service/NotificationService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -39,7 +40,8 @@ public class NotificationService {
                         .putData("time", LocalDateTime.now().toString())
                         .setNotification(alarm.toNotification())
                         .setToken(token);
-                    alarm.getAdditionalData().forEach(builder::putData);
+                    Optional.ofNullable(alarm.getAdditionalData())
+                            .ifPresent(additionalData -> additionalData.forEach(builder::putData));
 
                     return builder.build();
                 })
