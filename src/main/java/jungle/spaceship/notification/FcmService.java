cbd44d9af19508986da0db7079e153339c46850e
ssongone <41146676+ssongone@users.noreply.github.com>
2023-12-05 01:08:53 +0900

    Feat: FCM HTTP 레거시 버전 서비스 추가

--- src/main/java/jungle/spaceship/notification/FcmService.java
+++ src/main/java/jungle/spaceship/notification/FcmService.java
@@ -1,7 +1,14 @@
 package jungle.spaceship.notification;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class FcmService {
@@ -12,23 +19,37 @@ public class FcmService {
     @Value("${fcm.key}")
     private String SERVER_KEY;
 
-//    public void sendFcmMessage(String deviceToken, String title, String message, String additionalDataKey, String additionalDataValue) {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        headers.set("Authorization", "key=" + LEGACY_SERVER_KEY);
-//
-//        String requestBody = buildFcmRequestBody(deviceToken, title, message, additionalDataKey, additionalDataValue);
-//        System.out.println("FCM Request Body: " + requestBody);
-//
-//        HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
-//
-//        RestTemplate restTemplate = new RestTemplate();
-//        ResponseEntity<String> response = restTemplate.exchange(FCM_ENDPOINT, HttpMethod.POST, entity, String.class);
-//
-//        // Handle the response as needed
-//        System.out.println("FCM Response: " + response.getBody());
-//    }
+    public void sendFcmMessage(String firebaseToken, NotificationType type, String sender, String content) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "key=" + SERVER_KEY);
 
+        String requestBody = null;
+        try {
+            requestBody = makeResponse(firebaseToken, type, sender, content);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+        System.out.println("entity = " + entity);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(FCM_ENDPOINT, HttpMethod.POST, entity, String.class);
 
+        System.out.println("FCM Response: " + response.getBody());
+    }
+
+    private String makeResponse(String firebaseToken, NotificationType type, String sender, String content) throws JsonProcessingException {
+        Map<String, Object> jsonMap = new HashMap<>();
+
+        jsonMap.put("to", firebaseToken);
+
+        Map<String, Object> dataMap = type.apply(sender, content);
+        jsonMap.put("data", dataMap);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String result = objectMapper.writeValueAsString(jsonMap);
+
+        return result;
+    }
 
 }
---
+++ src/main/java/jungle/spaceship/notification/NotificationType.java
@@ -0,0 +1,58 @@
+package jungle.spaceship.notification;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+public enum NotificationType {
+
+    ADD_FAMILY_MEMBER((sender, message)-> {
+        Map<String, Object> map = new HashMap<>();
+        map.put("title", "Family");
+        map.put("message", "눌러서 확인해보세요");
+        return map;
+    }),
+    TMI((sender, message)-> {
+        Map<String, Object> map = new HashMap<>();
+        map.put("title", "TMI");
+        map.put("message", "눌러서 확인해보세요");
+        return map;
+    }),
+    CHAT((sender, message) -> {
+        Map<String, Object> map = new HashMap<>();
+        map.put("title", sender);
+        map.put("message", message);
+        return map;
+    }),
+    CALENDAR((sender, message)-> {
+        Map<String, Object> map = new HashMap<>();
+        map.put("title", "Calendar");
+        map.put("message", "눌러서 확인해보세요");
+        return map;
+    }),
+    PHOTO((sender, message)-> {
+        Map<String, Object> map = new HashMap<>();
+        map.put("title", "Photo");
+        map.put("message", "눌러서 확인해보세요");
+        return map;
+    }),
+    PLANT((sender, message)-> {
+        Map<String, Object> map = new HashMap<>();
+        map.put("title", "Plant");
+        map.put("message", "눌러서 확인해보세요");
+        return map;
+    });
+
+
+    private final BiFunction<String, String, Map<String, Object>> mapper;
+
+    NotificationType(BiFunction<String, String, Map<String, Object>> mapper) {
+        this.mapper = mapper;
+    }
+
+    public Map<String, Object> apply(String sender, String message) {
+        return mapper.apply(sender, message);
+    }
+
+
+}
