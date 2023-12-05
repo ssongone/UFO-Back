f88dc204c86fa46b000b319eb0872135f68d9b59
ssongone <41146676+ssongone@users.noreply.github.com>
2023-12-05 20:18:52 +0900

    Style: getPlant 응답 수정

--- src/main/java/jungle/spaceship/member/controller/PlantController.java
+++ src/main/java/jungle/spaceship/member/controller/PlantController.java
@@ -3,7 +3,9 @@ package jungle.spaceship.member.controller;
 import jungle.spaceship.member.controller.dto.PlantStateDto;
 import jungle.spaceship.member.entity.Plant;
 import jungle.spaceship.member.service.PlantService;
+import jungle.spaceship.response.ExtendedResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +17,8 @@ public class PlantController {
     private final PlantService plantService;
 
     @GetMapping("/plant")
-    public Plant getPlant() {
-        return plantService.getPlant();
+    public ExtendedResponse<Plant> getPlant() {
+        return new ExtendedResponse<>(plantService.getPlant(), HttpStatus.OK.value(), "");
     }
 
     @GetMapping("/api/1/point/{plantId}/{xp}")
